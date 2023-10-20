275f9f2391b118c69f6081d0099dd4849e9a7cfd
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-20 07:34:27 +0200

    feat: update imports at field analizer
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- engine/src/main/java/com/otaviojava/processor/FieldAnalyzer.java
+++ engine/src/main/java/com/otaviojava/processor/FieldAnalyzer.java
@@ -20,43 +20,29 @@ import com.github.mustachejava.MustacheFactory;
 import jakarta.nosql.Column;
 import jakarta.nosql.Entity;
 import jakarta.nosql.Id;
-import org.eclipse.jnosql.mapping.Convert;
 import org.eclipse.jnosql.mapping.Embeddable;
 import org.eclipse.jnosql.mapping.metadata.MappingType;
 
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.ProcessingEnvironment;
-import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 class FieldAnalyzer implements Supplier<String> {
 
     private static final String DEFAULT_TEMPLATE = "attribute.mustache";
-    private static final String GENERIC_TEMPLATE = "fieldgenericmetadata.mustache";
-    private static final Predicate<Element> IS_METHOD = el -> el.getKind() == ElementKind.METHOD;
-    private static final Function<Element, String> ELEMENT_TO_STRING = el -> el.getSimpleName().toString();
     private static final String NULL = "null";
     private final Element field;
     private final Mustache template;
 
-    private final Mustache genericTemplate;
     private final ProcessingEnvironment processingEnv;
     private final TypeElement entity;
 
@@ -66,7 +52,6 @@ class FieldAnalyzer implements Supplier<String> {
         this.processingEnv = processingEnv;
         this.entity = entity;
         this.template = createTemplate(DEFAULT_TEMPLATE);
-        this.genericTemplate = createTemplate(GENERIC_TEMPLATE);
     }
 
     @Override
@@ -75,11 +60,7 @@ class FieldAnalyzer implements Supplier<String> {
         Filer filer = processingEnv.getFiler();
         JavaFileObject fileObject = getFileObject(metadata, filer);
         try (Writer writer = fileObject.openWriter()) {
-            if(NULL.equals(metadata.getElementType())){
-                template.execute(writer, metadata);
-            } else {
-                genericTemplate.execute(writer, metadata);
-            }
+            template.execute(writer, metadata);
         } catch (IOException exception) {
             throw new ValidationException("An error to compile the class: " +
                     metadata.getQualified(), exception);
@@ -101,92 +82,18 @@ class FieldAnalyzer implements Supplier<String> {
         final String fieldName = field.getSimpleName().toString();
         final Predicate<Element> validName = el -> el.getSimpleName().toString()
                 .contains(ProcessorUtil.capitalize(fieldName));
-        final Predicate<String> hasGetterName = el -> el.equals("get" + ProcessorUtil.capitalize(fieldName));
-        final Predicate<String> hasSetterName = el -> el.equals("set" + ProcessorUtil.capitalize(fieldName));
-        final Predicate<String> hasIsName = el -> el.equals("is" + ProcessorUtil.capitalize(fieldName));
 
-        final List<Element> accessors = processingEnv.getElementUtils()
-                .getAllMembers(entity).stream()
-                .filter(validName.and(IS_METHOD).and(EntityProcessor.HAS_ACCESS))
-                .collect(Collectors.toList());
-
-        final TypeMirror typeMirror = field.asType();
-        String className;
-        String elementType = NULL;
-        boolean embeddable = false;
-        String collectionInstance = CollectionUtil.DEFAULT;
-        MappingType mappingType = MappingType.DEFAULT;
-        String supplierElement = null;
-
-
-        if (typeMirror instanceof DeclaredType declaredType) {
-            Element element = declaredType.asElement();
-            className = element.toString();
-            supplierElement = typeMirror.toString();
-            embeddable = isEmbeddable(declaredType);
-           collectionInstance = CollectionUtil.INSTANCE.apply(className);
-           elementType = elementType(declaredType);
-           mappingType = of(element, collectionInstance, collectionInstance);
-
-        } else {
-            className = typeMirror.toString();
-        }
         Column column = field.getAnnotation(Column.class);
         Id id = field.getAnnotation(Id.class);
-        Convert convert = field.getAnnotation(Convert.class);
 
-        List<ValueAnnotationModel> valueAnnotationModels = new ArrayList<>();
-        for (AnnotationMirror annotationMirror : field.getAnnotationMirrors()) {
-            DeclaredType annotationType = annotationMirror.getAnnotationType();
-            Map<? extends ExecutableElement, ? extends AnnotationValue> elementValues = annotationMirror.getElementValues();
-            Predicate<Map.Entry<? extends ExecutableElement, ? extends AnnotationValue>> isValueMethod =
-                    e -> e.getKey().toString().equals("value()");
-
-            List<String> defaultJNoSQL = List.of(Column.class.getName(),
-                    Id.class.getName(),
-                    Convert.class.getName());
-            Predicate<Map.Entry<? extends ExecutableElement, ? extends AnnotationValue>> isNotDefaultAnnotation = e -> !
-                    defaultJNoSQL.contains(annotationType.toString());
-            elementValues.entrySet().stream().filter(isNotDefaultAnnotation.and(isValueMethod)).findFirst().ifPresent(e -> {
-                String key = annotationType.toString()+".class";
-                String value = e.getValue().getValue().toString();
-                ValueAnnotationModel valueAnnotationModel = new ValueAnnotationModel(key, value);
-                valueAnnotationModels.add(valueAnnotationModel);
-            });
-        }
-
-        final boolean isId = id != null;
         final String packageName = ProcessorUtil.getPackageName(entity);
         final String entityName = ProcessorUtil.getSimpleNameAsString(this.entity);
         final String name = getName(fieldName, column, id);
 
-        final String getMethod = accessors.stream()
-                .map(ELEMENT_TO_STRING)
-                .filter(hasGetterName)
-                .findFirst().orElseThrow(generateGetterError(fieldName, packageName, entityName,
-                        "There is not valid getter method to the field: "));
-        final String setMethod = accessors.stream()
-                .map(ELEMENT_TO_STRING)
-                .filter(hasSetterName.or(hasIsName))
-                .findFirst().orElseThrow(generateGetterError(fieldName, packageName, entityName,
-                        "There is not valid setter method to the field: "));
-
         return FieldModel.builder()
                 .packageName(packageName)
                 .name(name)
-                .type(className)
                 .entity(entityName)
-                .reader(getMethod)
-                .writer(setMethod)
-                .fieldName(fieldName)
-                .id(isId)
-                .elementType(elementType)
-                .converter(convert)
-                .embeddable(embeddable)
-                .mappingType("MappingType." + mappingType.name())
-                .valueByAnnotation(valueAnnotationModels)
-                .collectionInstance(collectionInstance)
-                .supplierElement(supplierElement)
                 .build();
     }
 
--- engine/src/main/java/com/otaviojava/processor/FieldModel.java
+++ engine/src/main/java/com/otaviojava/processor/FieldModel.java
@@ -25,27 +25,7 @@ final class FieldModel extends BaseMappingModel {
 
     private String packageName;
     private String name;
-    private String type;
     private String entity;
-    private String reader;
-    private String writer;
-    private String fieldName;
-    private boolean id;
-    private String converter;
-
-    private String mappingType;
-
-    private String typeConverter;
-
-    private List<ValueAnnotationModel> valueByAnnotation;
-
-    private String elementType;
-
-    private boolean embeddable;
-
-    private String collectionInstance;
-
-    private String supplierElement;
 
     private FieldModel() {
     }
@@ -58,93 +38,20 @@ final class FieldModel extends BaseMappingModel {
         return name;
     }
 
-    public String getType() {
-        return type;
-    }
 
     public String getEntity() {
         return entity;
     }
 
-    public String getReader() {
-        return reader;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
 
     public String getQualified() {
         return packageName + "." + getClassName();
     }
 
     public String getClassName() {
-        return entity + ProcessorUtil.capitalize(fieldName) + "FieldMetaData";
+        return entity+"_" + ProcessorUtil.capitalize(name) + "Attribute";
     }
 
-    public String getFieldName() {
-        return fieldName;
-    }
-
-    public boolean isId() {
-        return id;
-    }
-
-
-    public String getConverter() {
-        return converter;
-    }
-
-    public String getMappingType() {
-        return mappingType;
-    }
-
-    public String getTypeConverter() {
-        return typeConverter;
-    }
-
-    public List<ValueAnnotationModel> getValueByAnnotation() {
-        return valueByAnnotation;
-    }
-
-    public String getElementType() {
-        return elementType;
-    }
-
-    public boolean isEmbeddable() {
-        return embeddable;
-    }
-
-    public String getCollectionInstance() {
-        return collectionInstance;
-    }
-
-    public String getSupplierElement() {
-        return supplierElement;
-    }
-
-
-    @Override
-    public String toString() {
-        return "FieldModel{" +
-                "packageName='" + packageName + '\'' +
-                ", name='" + name + '\'' +
-                ", type='" + type + '\'' +
-                ", entity='" + entity + '\'' +
-                ", reader='" + reader + '\'' +
-                ", writer='" + writer + '\'' +
-                ", fieldName='" + fieldName + '\'' +
-                ", id=" + id +
-                ", converter='" + converter + '\'' +
-                ", mappingType='" + mappingType + '\'' +
-                ", typeConverter='" + typeConverter + '\'' +
-                ", valueByAnnotation=" + valueByAnnotation +
-                ", elementType='" + elementType + '\'' +
-                ", embeddable=" + embeddable +
-                ", collectionInstance='" + collectionInstance + '\'' +
-                ", supplierElement='" + supplierElement + '\'' +
-                '}';
-    }
 
     public static FieldMetaDataBuilder builder() {
         return new FieldMetaDataBuilder();
@@ -157,9 +64,6 @@ final class FieldModel extends BaseMappingModel {
 
         private FieldMetaDataBuilder() {
             this.fieldModel = new FieldModel();
-            this.fieldModel.converter = "null";
-            this.fieldModel.mappingType = "null";
-            this.fieldModel.typeConverter = "null";
         }
 
         public FieldMetaDataBuilder packageName(String packageName) {
@@ -172,81 +76,10 @@ final class FieldModel extends BaseMappingModel {
             return this;
         }
 
-        public FieldMetaDataBuilder type(String type) {
-            this.fieldModel.type = type;
-            return this;
-        }
-
         public FieldMetaDataBuilder entity(String entity) {
             this.fieldModel.entity = entity;
             return this;
         }
-
-        public FieldMetaDataBuilder reader(String reader) {
-            this.fieldModel.reader = reader;
-            return this;
-        }
-
-        public FieldMetaDataBuilder writer(String writer) {
-            this.fieldModel.writer = writer;
-            return this;
-        }
-
-        public FieldMetaDataBuilder fieldName(String fieldName) {
-            this.fieldModel.fieldName = fieldName;
-            return this;
-        }
-
-        public FieldMetaDataBuilder id(boolean id) {
-            this.fieldModel.id = id;
-            return this;
-        }
-
-        public FieldMetaDataBuilder converter(Convert converter) {
-            if (Objects.nonNull(converter)) {
-                try {
-                    this.fieldModel.converter = String.format("new %s();", converter.value().getName());
-                    this.fieldModel.typeConverter = converter.value().getName().concat(".class");
-                } catch (MirroredTypeException exception) {
-                    TypeMirror typeMirror = exception.getTypeMirror();
-                    this.fieldModel.converter = String.format("new %s()", typeMirror);
-                    this.fieldModel.typeConverter = typeMirror.toString().concat(".class");
-                }
-
-            }
-            return this;
-        }
-
-        public FieldMetaDataBuilder mappingType(String mappingType) {
-            this.fieldModel.mappingType = mappingType;
-            return this;
-        }
-
-        public FieldMetaDataBuilder valueByAnnotation(List<ValueAnnotationModel> valueByAnnotation) {
-            this.fieldModel.valueByAnnotation = valueByAnnotation;
-            return this;
-        }
-
-        public FieldMetaDataBuilder elementType(String elementType) {
-            this.fieldModel.elementType = elementType;
-            return this;
-        }
-
-        public FieldMetaDataBuilder embeddable(boolean embeddable) {
-            this.fieldModel.embeddable = embeddable;
-            return this;
-        }
-
-        public FieldMetaDataBuilder collectionInstance(String collectionInstance) {
-            this.fieldModel.collectionInstance = collectionInstance;
-            return this;
-        }
-
-        public FieldMetaDataBuilder supplierElement(String supplierElement) {
-            this.fieldModel.supplierElement = supplierElement;
-            return this;
-        }
-
 
         public FieldModel build() {
            return fieldModel;
