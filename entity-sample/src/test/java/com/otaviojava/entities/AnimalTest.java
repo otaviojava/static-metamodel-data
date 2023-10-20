001b1b8f024a18466cc0712c4163102b3c1c6075
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-20 13:49:41 +0200

    style:  remove imports
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- entity-sample/src/test/java/com/otaviojava/entities/AnimalTest.java
+++ entity-sample/src/test/java/com/otaviojava/entities/AnimalTest.java
@@ -2,7 +2,6 @@ package com.otaviojava.entities;
 
 import jakarta.data.Sort;
 import jakarta.data.metamodel.Attribute;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
