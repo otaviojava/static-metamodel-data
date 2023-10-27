f403d666824f3a4b60a2eadb87c2e941f84496c0
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-27 17:26:32 +0100

    update
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/com/otaviojava/entities/Person.java
+++
@@ -1,2 +0,0 @@
-package com.otaviojava.entities;public record Person() {
-}
--- api/src/main/java/com/otaviojava/entities/Zoo.java
+++
@@ -1,8 +0,0 @@
-package com.otaviojava.entities;
-
-import jakarta.data.repository.PageableRepository;
-import jakarta.data.repository.Repository;
-
-@Repository
-public interface Zoo extends PageableRepository<Animal, String>{
-}
