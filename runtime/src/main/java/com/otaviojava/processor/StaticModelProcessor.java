21d231406fbf9f672c344fa4389ccb9e561cd81a
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-30 22:44:05 +0000

    feat: create logger
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- runtime/src/main/java/com/otaviojava/processor/StaticModelProcessor.java
+++ runtime/src/main/java/com/otaviojava/processor/StaticModelProcessor.java
@@ -1,4 +1,17 @@
 package com.otaviojava.processor;
 
+import jakarta.data.metamodel.StaticMetamodel;
+
+import java.util.logging.Logger;
+
 class StaticModelProcessor {
+
+    private static final Logger LOGGER = Logger.getLogger(StaticModelProcessor.class.getName());
+    public <T> T process(Class<T> type) {
+        StaticMetamodel metamodel = type.getAnnotation(StaticMetamodel.class);
+        Class<?> entity = metamodel.value();
+
+
+        return null;
+    }
 }
