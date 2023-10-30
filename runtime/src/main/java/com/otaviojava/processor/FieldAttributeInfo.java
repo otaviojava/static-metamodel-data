6fb978ab386923b0fb0a5b61ebb4b388859135b2
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-30 21:56:05 +0000

    feat: create field attribute info using field
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- runtime/src/main/java/com/otaviojava/processor/FieldAttributeInfo.java
+++ runtime/src/main/java/com/otaviojava/processor/FieldAttributeInfo.java
@@ -2,6 +2,11 @@ package com.otaviojava.processor;
 
 import jakarta.data.Sort;
 import jakarta.data.metamodel.AttributeInfo;
+import jakarta.nosql.Column;
+import jakarta.nosql.Id;
+
+import java.lang.reflect.Field;
+import java.util.Objects;
 
 public record FieldAttributeInfo(String name) implements AttributeInfo {
     @Override
@@ -27,5 +32,18 @@ public record FieldAttributeInfo(String name) implements AttributeInfo {
     @Override
     public String name() {
         return name;
+    }
+
+    public static FieldAttributeInfo of(Field field){
+        Objects.requireNonNull(field, "field cannot be null");
+        var id = field.getAnnotation(Id.class);
+        if (id != null) {
+            return new FieldAttributeInfo(id.value().isBlank() ? field.getName() : id.value());
+        }
+        var column = field.getAnnotation(Column.class);
+        if (column != null) {
+            return new FieldAttributeInfo(column.value().isBlank() ? field.getName() : column.value());
+        }
+        return new FieldAttributeInfo(field.getName());
     }
 }
