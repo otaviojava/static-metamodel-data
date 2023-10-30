1dc1b1f9523031f42edc7237737e18ebee0d4cf0
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-30 22:38:41 +0000

    test: create metadataprocessor
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- runtime/src/test/java/com/otaviojava/processor/MetaDataProcessorTest.java
+++ runtime/src/test/java/com/otaviojava/processor/MetaDataProcessorTest.java
@@ -1,17 +1,10 @@
 package com.otaviojava.processor;
 
-import org.jboss.weld.junit5.WeldJunit5Extension;
-import org.jboss.weld.junit5.auto.AddExtensions;
-import org.jboss.weld.junit5.auto.AddPackages;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(WeldJunit5Extension.class)
-@AddPackages(MetaDataProcessor.class)
-@AddExtensions(MetaDataProcessor.class)
-@AddPackages(Person.class)
+
 class MetaDataProcessorTest {
 
     @Test
