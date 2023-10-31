4447d061a506a8eae3822ae0b4dc45c063c628db
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-31 13:25:05 +0000

    test: create init scenario
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- runtime/src/main/java/com/otaviojava/processor/StaticModelProcessor.java
+++ runtime/src/main/java/com/otaviojava/processor/StaticModelProcessor.java
@@ -5,6 +5,7 @@ import jakarta.data.metamodel.StaticMetamodel;
 
 import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
@@ -12,17 +13,18 @@ import java.util.stream.Stream;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toMap;
 
-class StaticModelProcessor<T> implements Consumer<Class<T>> {
+class StaticModelProcessor implements Consumer<Class<?>> {
 
     private static final Logger LOGGER = Logger.getLogger(StaticModelProcessor.class.getName());
 
-    static final StaticModelProcessor<?> INSTANCE = new StaticModelProcessor<>();
+    static final StaticModelProcessor INSTANCE = new StaticModelProcessor();
 
     private StaticModelProcessor() {
     }
 
     @Override
-    public void accept(Class<T> type) {
+    public void accept(Class<?> type) {
+        Objects.requireNonNull(type, "type is required");
         LOGGER.info("Processing entity: " + type);
         StaticMetamodel metamodel = type.getAnnotation(StaticMetamodel.class);
         Class<?> entity = metamodel.value();
--- runtime/src/test/java/com/otaviojava/processor/StaticModelProcessorTest.java
+++ runtime/src/test/java/com/otaviojava/processor/StaticModelProcessorTest.java
@@ -1,7 +1,25 @@
 package com.otaviojava.processor;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StaticModelProcessorTest {
+    @Test
+    public void shouldReturnNPE() {
+        assertThrows(NullPointerException.class, () -> {
+            StaticModelProcessor.INSTANCE.accept(null);
+        });
+    }
+
+    @Test
+    public void shouldInitializeStaticModelProcessor() {
+        Assertions.assertNull(Person_.id);
+        Assertions.assertNull(Person_.name);
+        StaticModelProcessor.INSTANCE.accept(Person_.class);
+        Assertions.assertNotNull(Person_.id);
+        Assertions.assertNotNull(Person_.name);
+    }
 
 }
